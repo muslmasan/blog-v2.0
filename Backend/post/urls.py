f709b0a7562f6ab6d607cba2f12f125fd13ba97e
muslmasan <muslmasan3@gmail.com>
2024-10-21 11:03:30 +0200

    updating the endpoint

--- Backend/post/urls.py
+++ Backend/post/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostDetailView, PostListCreateView
+from .views import PostDetailView, PostListView, PostCreateView
 
 urlpatterns = [
-    path('posts/', PostListCreateView.as_view(), name='post-list-create'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail')
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/created/', PostCreateView.as_view(), name='post-create')
 ]
--- Backend/post/views.py
+++ Backend/post/views.py
@@ -3,15 +3,20 @@ from django.contrib.auth.models import User
 from rest_framework import generics , viewsets
 from.serializers import PostSerializer 
 from .models import Post
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny, IsAuthenticated
 
 
-class PostListCreateView(generics.ListCreateAPIView):
+class PostListView(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly] 
+
+class PostCreateView(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
